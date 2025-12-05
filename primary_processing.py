aff5f6f24cec53cf76be61542da89fef66210898
sologuboved <sologuboved@gmail.com>
2025-12-06 00:25:08 +0300

    WIP

--- primary_processing.py
+++ primary_processing.py
@@ -40,7 +40,10 @@ def from_csv(drop=False):
     print(f"mots_temp.csv -> {DB_NAME}.{COLLOCATIONS}")
     target = MongoClient(LOCALHOST, PORT)[DB_NAME][COLLOCATIONS]
     print(f"Initially, {target.estimated_document_count()} entries")
-    target.insert_many(map(lambda x: {'': None}.get(x, x), [read_csv('mots_temp.csv', as_dict=True)]))
+    target.insert_many(map(
+        lambda x: dict(x, **{'trad': {'': None}.get(x['trad'], x['trad'])}),
+        read_csv('mots_temp.csv', as_dict=True),
+    ))
     print(f"Finally, {target.estimated_document_count()} entries")
 
 
