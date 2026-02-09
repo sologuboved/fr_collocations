a0148fbc1329d47d6d3c70ddd48a52271bbf8984
sologuboved <sologuboved@gmail.com>
2026-02-10 01:40:41 +0300

    rename and other improvements

--- upd_db.py
+++ upd_db.py
@@ -36,19 +36,28 @@ def by_tag(tag, drop=False):
     print(f"Finally, {target.estimated_document_count()} entries")
 
 
-def from_csv(drop=False):
+def from_csv(coll_name=COLLOCATIONS, drop=False):
     print(f"mots_temp.csv -> {DB_NAME}.{COLLOCATIONS}")
-    target = MongoClient(LOCALHOST, PORT)[DB_NAME][COLLOCATIONS]
+    target = MongoClient(LOCALHOST, PORT)[DB_NAME][coll_name]
     print(f"Initially, {target.estimated_document_count()} entries")
     if drop:
         target.drop()
-    target.insert_many(map(
-        lambda x: dict(x, **{'trad': {'': None}.get(x['trad'], x['trad'])}),
-        read_csv('mots_temp.csv', as_dict=True),
-    ))
+    entries = list()
+    for row in read_csv('mots_temp.csv', as_dict=True):
+        mot = row['mot']
+        if target.find_one({'mot': mot}):
+            print(f"'{mot}' est déjà présent ; on l'omet")
+            continue
+        else:
+            row['trad'] = row['trad'] or None
+            entries.append(row)
+    if entries:
+        target.insert_many(entries)
     print(f"Finally, {target.estimated_document_count()} entries")
 
 
 if __name__ == '__main__':
     # by_tag('phrases')
-    from_csv()
+    from_csv(
+        # coll_name=COLLOCATIONS + '_test',
+    )
