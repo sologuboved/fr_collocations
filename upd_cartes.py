8f1b6b38ade8e9baaf571fa6dfaed9caa3aa3fc6
sologuboved <sologuboved@gmail.com>
2026-02-20 18:55:20 +0300

    upd

--- upd_cartes.py
+++ upd_cartes.py
@@ -3,13 +3,16 @@ import subprocess
 from helpers import dump_utf_json, read_csv
 
 
-def csv_to_json():
+def main():
     dump_utf_json(list(read_csv('cartes.csv', as_dict=True)), 'cartes.json')
-    subprocess.run(
-        "sudo cp cartes.json /var/www/html/cartes/fran.json",
-        shell=True,
-    )
+    for command in (
+        # "git add cartes.json",
+        # 'git commit -m "upd"',
+        "git push origin main",
+    ):
+        print(command + '...')
+        subprocess.run(command, shell=True)
 
 
 if __name__ == '__main__':
-    csv_to_json()
+    main()
