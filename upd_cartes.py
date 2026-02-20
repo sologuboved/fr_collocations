dd5991213c936aa83f5311610c14c8a3a2ee0ccc
sologuboved <sologuboved@gmail.com>
2026-02-20 20:05:52 +0300

    upd

--- upd_cartes.py
+++ upd_cartes.py
@@ -6,8 +6,8 @@ from helpers import dump_utf_json, read_csv
 def main():
     dump_utf_json(list(read_csv('cartes.csv', as_dict=True)), 'cartes.json')
     for command in (
-        # "git add cartes.json",
-        # 'git commit -m "upd"',
+        "git add cartes.json",
+        'git commit -m "upd"',
         "git push origin main",
     ):
         print(command + '...')
