f88de43e8d337f21bd8b70493da453f48a4672ae
sologuboved <sologuboved@gmail.com>
2026-02-22 17:12:31 +0300

    WIP

--- upd_cartes.py
+++ upd_cartes.py
@@ -3,8 +3,11 @@ import subprocess
 from helpers import dump_utf_json, read_csv
 
 
-def main():
-    dump_utf_json(list(read_csv('cartes.csv', as_dict=True)), 'cartes.json')
+def main(sort=True):
+    cartes = list(read_csv('cartes.csv', as_dict=True))
+    if sort:
+        cartes.sort(key=lambda x: (len(x['trad']), x['mot'],))
+    dump_utf_json(cartes, 'cartes.json')
     for command in (
         "git add cartes.json",
         'git commit -m "upd"',
