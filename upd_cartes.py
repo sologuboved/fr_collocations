5d86d95584b3702ee31632e4a403c9f58eca8256
sologuboved <sologuboved@gmail.com>
2026-03-29 18:21:56 +0300

    WIP

--- upd_cartes.py
+++ upd_cartes.py
@@ -5,7 +5,7 @@ from helpers import dump_utf_json, read_csv
 
 def main(from_csv=False, sort=True):
     if from_csv:
-        cartes = list(read_csv('cartes.csv', as_dict=True))
+        cartes = [zip(('mot', 'trad'), map(str.strip, row)) for row in read_csv('cartes.csv', as_dict=False)]
         if sort:
             cartes.sort(key=lambda x: (len(x['trad']), x['mot'],))
         dump_utf_json(cartes, 'cartes.json')
