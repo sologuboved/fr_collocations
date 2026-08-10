dc1379a69c49b8520e5c5424ec32e954ae1e39a9
sologuboved <sologuboved@gmail.com>
2026-08-10 11:37:11 +0300

    WIP

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DEFAULT_NUM = 15
 FILE_PATH = 'collocations.txt'
 LOCALHOST = 'localhost'
 PORT = 27017
-URL_CITATIONS = 'https://github.com/sologuboved/fr_collocations/blob/main/citations.json'
-URL_COLLOCATIONS = 'https://github.com/sologuboved/fr_collocations/blob/main/collocations.json'
+URL_CITATIONS = 'https://raw.githubusercontent.com/sologuboved/fr_collocations/refs/heads/main/citations.json'
+URL_COLLOCATIONS = 'https://raw.githubusercontent.com/sologuboved/fr_collocations/refs/heads/main/collocations.json'
 
 FROM_DB = False
